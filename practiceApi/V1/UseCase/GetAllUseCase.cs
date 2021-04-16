2bb03e418372d8e42c60bb2f42f00ea70fa41cad
humulla <humairaa.mulla@hackney.gov.uk>
2021-04-16 15:41:53 +0100

    rename api to practice api

---
+++ practiceApi/V1/UseCase/GetAllUseCase.cs
@@ -0,0 +1,22 @@
+using practiceApi.V1.Boundary.Response;
+using practiceApi.V1.Factories;
+using practiceApi.V1.Gateways;
+using practiceApi.V1.UseCase.Interfaces;
+
+namespace practiceApi.V1.UseCase
+{
+    //TODO: Rename class name and interface name to reflect the entity they are representing eg. GetAllClaimantsUseCase
+    public class GetAllUseCase : IGetAllUseCase
+    {
+        private readonly IExampleGateway _gateway;
+        public GetAllUseCase(IExampleGateway gateway)
+        {
+            _gateway = gateway;
+        }
+
+        public ResponseObjectList Execute()
+        {
+            return new ResponseObjectList { ResponseObjects = _gateway.GetAll().ToResponse() };
+        }
+    }
+}
